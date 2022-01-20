cc32e6c97eff2e5f08f33e7be130c62e664cc3ff
Diego Gómez González <93382813+diegogomezcampusfp@users.noreply.github.com>
2022-01-20 10:24:29 +0100

    Update index.php

--- index.php
+++ index.php
@@ -1,15 +1,42 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Heroku</title>
-</head>
-<body>
-    <?php
-    echo "<h2>Variables y constantes</h2>";
-    
+<?php
+        echo "<h2>Variables y constantes</h2>";
+        define("DATO", "un dato de ejemplo");  // para declarar una constante tambien se puede hacer con la palabra const
+        echo "La constante dato es " . DATO;
+        $numero = 5;
+        $numero++;
+        echo "<p>El numero es " . $numero . "</p>"; 
+        echo "El numero es " . $numero++ . "</p>";
+        echo "<h3>Operadores</h3>";
+        $numero1=10;
+        $numero2=6;
+        if($numero1 >=$numero2){ // operador de comparacion 
+            echo "<p>El primer numero es el mayor</p>";
+        } else {
+            echo "<p>el primer numero es menor</p>";
+        }
+        $aprobado= true;
+        if($aprobado){
+            echo "Has aprobado";
+
+        } else {
+            echo "No apto";
+        }
+        
+        $nota="8";
+        if($nota==8){
+            echo "Tu nota es un notable";
+        } else {
+            echo "Tu nota no es un ocho";
+        }
+        echo "<hr>";
+        echo "<h3>Operadores</h3>";
+        for($i=0; $i<=10; $i++){ //recorrer un array con un for consume mucha memoria
+            echo $i . "-";
+        }
+        $a=0;
+        echo "<br />";
+        while ($a <=10){
+            echo $a++."-";
+        }
+
     ?>
-</body>
-</html>
